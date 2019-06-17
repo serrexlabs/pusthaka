2a579e87a30f02c28c175bdc5c9356752b527a21
Sanath Samarasinghe <sanath.samarasinghe@outlook.com>
2019-06-17 19:05:13 +0530

    init project

---
+++ webpack.rules.js
@@ -0,0 +1,43 @@
+module.exports = [
+  // Add support for native node modules
+  {
+    test: /\.node$/,
+    use: 'node-loader',
+  },
+  {
+    test: /\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      options: {
+        outputAssetBase: 'native_modules',
+      },
+    },
+  },
+  {
+    test: /\.(js)$/,
+    exclude: /(node_modules|.webpack)/,
+    use: 'babel-loader'
+  },
+  {
+    test: /\.(css)$/,
+    exclude: /(node_modules|.webpack)/,
+    use: ['style-loader', 'css-loader']
+  },
+  // Put your webpack loader rules in this array.  This is where you would put
+  // your ts-loader configuration for instance:
+  /**
+   * Typescript Example:
+   *
+   * {
+   *   test: /\.tsx?$/,
+   *   exclude: /(node_modules|.webpack)/,
+   *   loaders: [{
+   *     loader: 'ts-loader',
+   *     options: {
+   *       transpileOnly: true
+   *     }
+   *   }]
+   * }
+   */
+];
