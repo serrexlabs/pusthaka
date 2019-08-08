bff0f7168d2be9c0281e406e121f7d156c078b29
Sanath Samarasinghe <sanath@serrexlabs.com>
2019-08-08 17:59:32 +0530

    add drawer

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -7,23 +7,61 @@ import {
 } from '@atlaskit/navigation-next';
 import GlobalNavigation from '@atlaskit/global-navigation';
 import { navTheme } from '../theme';
+import Drawer from '@atlaskit/drawer';
+import {useState} from "react";
+
 const { dialog } = require('electron').remote;
 
-const Global = () => (
-    <GlobalNavigation
-        productIcon={EmojiAtlassianIcon}
-        productHref="#"
-        onProductClick={() => console.log('product clicked')}
-        onCreateClick={() => {
-            dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'], filters: [
-                    { name: 'Books', extensions: ['pdf'] },
-                ]})
-        }}
-        onSearchClick={() => console.log('search clicked')}
-        onSettingsClick={() => console.log('settings clicked')}
-        loginHref="#login"
-    />
+const DrawerContent = ({
+                           drawerTitle,
+                           drawerBody,
+                       }: {
+    drawerTitle: string,
+    drawerBody: string,
+}) => (
+    <div>
+        <h1>{drawerTitle}</h1>
+        <div>{drawerBody}</div>
+
+    </div>
 );
+
+const Global = () => {
+    const [isCreateDrawer, openCreateDrawer] = useState(true);
+
+    const renderDrawer = () => (
+        <DrawerContent
+            drawerTitle="Starred drawer"
+            drawerBody="Can be controlled by passing the onStarredClick prop"
+        />
+    );
+
+    return (
+        <GlobalNavigation
+            productIcon={EmojiAtlassianIcon}
+            productHref="#"
+            onProductClick={() => console.log('product clicked')}
+            onCreateClick={() => {
+  /*              dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'], filters: [
+                        { name: 'Books', extensions: ['pdf'] },
+                    ]})*/
+               // openCreateDrawer(true);
+            }}
+
+            onSearchClick={() => openCreateDrawer(true)}
+            searchTooltip="Search (\)"
+            isSearchDrawerOpen={isCreateDrawer}
+            searchDrawerContents={renderDrawer}
+            onSearchDrawerClose={() => openCreateDrawer(false)}
+            onSearchDrawerCloseComplete={()=> console.log("closed")}
+
+
+
+            onSettingsClick={() => console.log('settings clicked')}
+            loginHref="#login"
+        />
+    );
+}
 
 export default () => (
     <NavigationProvider>
@@ -33,12 +71,12 @@ export default () => (
                 mode: navTheme,
             })}
         >
-        <LayoutManager
-            globalNavigation={Global}
-            productNavigation={() => null}
-            containerNavigation={() => null}
-        >
-        </LayoutManager>
+            <LayoutManager
+                globalNavigation={Global}
+                productNavigation={() => null}
+                containerNavigation={() => null}
+            >
+            </LayoutManager>
         </ThemeProvider>
     </NavigationProvider>
 );
