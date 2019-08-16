71d5bdec6d93556716b9de75f982c063e567af59
Sanath Samarasinghe <sanath@serrexlabs.com>
2019-08-17 01:36:52 +0530

    fix

--- src/services/book-service.spec.ts
+++ src/services/book-service.spec.ts
@@ -5,10 +5,12 @@ import BookService from "./book-service";
 describe("BookService", (): void => {
   const sandbox = sinon.createSandbox();
   let service: BookService;
+  const now: any = new Date(2019, 8, 15, 10, 1, 2, 2);
+  const dateHash = Math.floor(now).toString(16);
 
   beforeEach(() => {
     service = new BookService();
-    sandbox.useFakeTimers(new Date(2019, 8, 15, 10, 1, 2, 2));
+    sandbox.useFakeTimers(now);
   });
 
   afterEach((): void => {
@@ -18,6 +20,6 @@ describe("BookService", (): void => {
   it("will generate unique and universal id for give details", (): void => {
     const id = service.generateId("author", "title");
 
-    expect(id).to.equal("16d33303b72.5e992120eb81ad9144ea0493ea34cfba8447140d");
+    expect(id).to.equal(`${dateHash}.5e992120eb81ad9144ea0493ea34cfba8447140d`);
   });
 });
