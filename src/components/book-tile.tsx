05e1d16c2a26eb3d7481d672f014df9c6d427fba
Sanath Samarasinghe <sanath@serrexlabs.com>
2019-08-19 02:04:12 +0530

    wip

--- src/components/book-tile.tsx
+++ src/components/book-tile.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { userDataPath } from "../core/config";
 
 interface Props {
   id: string;
@@ -8,16 +9,16 @@ interface Props {
   /* thumbnail: string; */
 }
 
+function imagePath(id: string) {
+  return `file://${userDataPath}/thumbs/${id}.jpeg`;
+}
+
 function BookTile(props: Props): JSX.Element {
   return (
     <div key={props.id} className="container__col-3" style={{ width: "200px" }}>
       <p className="title">{props.title}</p>
       <p>{props.author}</p>
-      <img
-        style={{ width: "100px" }}
-        src={`file:///home/sanath/.config/pusthaka/thumbs/${props.id}.jpeg`}
-        alt=""
-      />
+      <img style={{ width: "100px" }} src={imagePath(props.id)} alt="" />
     </div>
   );
 }
