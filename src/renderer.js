bc435ef9b9b6fa7f4e27d2c1a5a4c6de216c5cf7
Sanath Samarasinghe <sanath@serrexlabs.com>
2019-08-21 08:43:55 +0530

    remove redux

--- src/renderer.js
+++ src/renderer.js
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import Router from "./Router";
-import configureStore from "./store/configure-store";
 import bootstrap from "./bootstrap";
 
 bootstrap();
 
-const store = configureStore({});
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Router />
-  </Provider>,
-  document.getElementById("root"),
-);
+ReactDOM.render(<Router />, document.getElementById("root"));
