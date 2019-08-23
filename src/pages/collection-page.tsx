5af1e49fa38d8ce440e04a6577a1d030d79a60f9
Sanath Samarasinghe <sanath@serrexlabs.com>
2019-08-23 08:46:40 +0530

    wip

--- src/pages/collection-page.tsx
+++ src/pages/collection-page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import RouterProps from "../types/router-props";
+import PDFViewer from "../components/pdf-viewer";
 
 // eslint-disable-next-line no-unused-vars
 function CollectionPage(props: RouterProps): JSX.Element {
-  return <h1>CollectionPage</h1>;
+  return <PDFViewer src="file:///home/sanath/.config/pusthaka/books/16cbc6e92ed.a8b3128527c55c2efa1314d23e8690d12ae28a45.pdf"/>;
 }
 
 export default CollectionPage;
